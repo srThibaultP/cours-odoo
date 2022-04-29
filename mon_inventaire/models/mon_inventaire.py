f8b18567e9eed6e8af9c30283581db8a3ef3347d
Thibault PECH <thibaultpech@outlook.com>
2022-04-29 09:13:21 +0200

    kanban ok

--- mon_inventaire/models/mon_inventaire.py
+++ mon_inventaire/models/mon_inventaire.py
@@ -11,6 +11,7 @@ class Materiel(models.Model):
     type = fields.Selection([('Switch', 'SWITCH'),('Routeur', 'ROUTEUR')])
     marque_id = fields.Many2one('mon_inventaire.marque', string='Marque')
     responsable_id = fields.Many2one('mon_inventaire.responsable', string='Responsable')
+    responsable_id_full_name = fields.Char(related='responsable_id.full_name', string='Responsable')
     description = fields.Text('Liste du matériel')
 
 class Marque(models.Model):
@@ -25,8 +26,14 @@ class Responsable(models.Model):
     _name = 'mon_inventaire.responsable'
     _description = 'mon_inventaire.responsable'
     
-    name = fields.Char(string="Nom du responsable", required = True)
-    prenom = fields.Char(string="Prénom du responsable", required = True)
-    telephone = fields.Char(string="Téléphone du responsable", required = True)
-    email = fields.Char(string="Email du responsable", required = True)
+    name = fields.Char(string="Nom", required = True)
+    prenom = fields.Char(string="Prénom", required = True)
+    full_name = fields.Char(compute='_compute_full_name', string='Nom complet')
+    telephone = fields.Char(string="Téléphone", required = True)
+    email = fields.Char(string="Email", required = True)
     materiel_ids = fields.One2many('mon_inventaire.materiel', 'responsable_id', string='Matériel associé')
+
+    @api.depends('name', 'prenom')
+    def _compute_full_name(self):
+        for rec in self:
+            rec.full_name = rec.name + ' ' + rec.prenom
